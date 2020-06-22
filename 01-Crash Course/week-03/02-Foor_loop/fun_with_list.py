85a1de5b7885998576a80a1efbf17ddddeca83e4
VARUN KUMAR <varunjha089@gmail.com>
2020-06-23 03:27:26 +0530

    Doin some fun with python-list using for loop.

---
+++ 01-Crash Course/week-03/02-Foor_loop/fun_with_list.py
@@ -0,0 +1,16 @@
+def validate_users(users):
+    for user in [users]:
+        print(user)
+
+
+def validate_users_two(users):
+  for user in users:
+    print(user)
+
+
+validate_users("purplecat")
+validate_users_two("second_one")
+
+
+
+
