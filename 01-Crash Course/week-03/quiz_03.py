24200b0071add7a8fc90e3b21272f588743953a7
VARUN KUMAR <varunjha089@gmail.com>
2020-06-28 11:21:50 +0530

    Week-03 quiz passed with 100%.

--- 01-Crash Course/week-03/quiz_03.py
+++ 01-Crash Course/week-03/quiz_03.py
@@ -14,7 +14,7 @@
 # >>> failure to initialize a variable
 
 #  Question 2
-
+#
 # def show_letters(word):
 #     for letter in word:
 #         print(letter)
@@ -55,29 +55,50 @@
 # Should print the multiplication table shown above
 
 # Question 5
+#
+# def counter(start, stop):
+#     x = start
+#     if x > stop:
+#         return_string = "Counting down: "
+#         while x >= stop:
+#             return_string += str(x)
+#             if isinstance(int(return_string[-1]), int):
+#                 if int(return_string[-1]) != stop:
+#                     return_string += ","
+#             x -= 1
+#     else:
+#         return_string = "Counting up: "
+#         while x <= stop:
+#             return_string += str(x)
+#             if isinstance(int(return_string[-1]), int):
+#                 if int(return_string[-1]) != int(str(stop)[-1]):
+#                     return_string += ","
+#             x += 1
+#     return return_string
+#
+#
+# print(counter(1, 10))  # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
+# print(counter(2, 1))  # Should be "Counting down: 2,1"
+# print(counter(5, 5))  # Should be "Counting up: 5"
+# print(counter(10, 1))  # Should be "Counting down: 2,1"
 
-def counter(start, stop):
-    x = start
-    if x > stop:
-        return_string = "Counting down: "
-        while x >= stop:
-            return_string += str(x)
-            if isinstance(return_string[::-1], int)	:
-                return_string += ","
-            return_string
-    else:
-        return_string = "Counting up: "
-        while x <= stop:
-            return_string += str(x)
-            if isinstance(return_string[::-1], int):
-                return_string += ","
-            return_string
-    return return_string
+# Question 6
+#
+def even_numbers(maximum):
+    return_string = ""
+    if maximum != 0 and maximum != 1:
+        for x in range(maximum + 1):
+            if x != 0:
+                if x % 2 == 0:
+                    return_string += str(x) + " "
+    return return_string.strip()
 
 
-print(counter(1, 10))  # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
-print(counter(2, 1))  # Should be "Counting down: 2,1"
-print(counter(5, 5))  # Should be "Counting up: 5"
+print(even_numbers(6))  # Should be 2 4 6
+print(even_numbers(10))  # Should be 2 4 6 8 10
+print(even_numbers(1))  # No numbers displayed
+print(even_numbers(3))  # Should be 2
+print(even_numbers(0))  # No numbers displayed
 
 # Question 8
 
@@ -85,13 +106,13 @@ print(counter(5, 5))  # Should be "Counting up: 5"
 #     print(x)
 
 # Question 9
-
+#
 # for x in range(10):
 #     for y in range(x):
 #         print(y)
 
 # Question 10
-
+#
 # def votes(params):
 #     for vote in params:
 #         print("Possible option:" + vote)
